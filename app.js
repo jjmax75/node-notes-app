a63ee144e1fae51b005ec691cc07fd2e054057bf
John Behan <behanj@gmail.com>
2019-06-28 09:52:47 +0100

    get notes function

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
+const getNotes = require('./notes');
 
-// fs.writeFileSync("notes.txt", "this file was created by Node.js");
-
-fs.appendFileSync('notes.txt', 'so it was');
+console.log(getNotes());
